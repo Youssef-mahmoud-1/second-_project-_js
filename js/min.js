913303e2759f944d975fe165617db32210ee15bc
Youssef-mahmoud-1 <y.mahmoud292006@gmail.com>
2025-09-16 05:45:40 +0300

    Add files via upload

---
+++ js/min.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+function convertDollar(){
+
+   var dollar = document.getElementById("dollar").value;
+
+   var result = document.getElementById("result");
+
+if(dollar==""){
+   
+        result.innerHTML="Enter Data"
+        return false;
+    }else if(isNaN(dollar)){
+        result.innerHTML="Enter Number Not Text"
+        return false;
+    }else if (dollar<0){
+         result.innerHTML="Enter postive Number"
+         return false;
+    }else if (dollar==0){
+         result.innerHTML="Enter Number Rather Than Zero"
+         return false;
+    }else {
+         result.innerHTML= dollar*48 +" Egyption Pound"
+         return false;
+    }
+
+}
